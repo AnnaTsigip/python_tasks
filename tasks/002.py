e017d338d051d588f880adebedaff0c6b6bddb69
Anna <atsigip@gmail.com>
2022-04-15 20:20:41 +0300

    max from 5

--- tasks/002.py
+++ tasks/002.py
@@ -1,19 +1,23 @@
-# 2.Вывести на экран числа от -N до N
+# 2. Найти максимальное из пяти чисел
+"""
+list = [5, 7, 12, 6, 1]
+print(max(list))
+"""
 
-N = int(input('введите N: '))
-count = int(-N)
 
-while (count < N+1):
 
-  print(count,end=' ')
-  count+=1
-
- 
- 
- # с использованием модуля числа, если ввели отрицательное
-
-# Вывести на экран числа от -N до N
-
-N = abs(-10)
-for i in range(-N, N + 1):
-    print(i, end = ' ')
+a = int(input('Введите a: '))
+b = int(input('Введите b: '))
+c = int(input('Введите c: '))
+d = int(input('Введите d: '))
+e = int(input('Введите e: '))
+max = a
+if b > max:
+    max =b
+if c > max:
+    max = c
+if d > max:
+    max = d
+if e > max:
+    max = e
+print(f'максимальное число = {max}')
