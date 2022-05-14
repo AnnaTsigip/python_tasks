4a79ff1c6abb320a695b9c0c6910af1cd66fe49b
Anna <atsigip@gmail.com>
2022-05-14 14:45:11 +0300

    list of non-repeating elements

--- home_work/003.py
+++ home_work/003.py
@@ -1,4 +1,4 @@
-#3. Задайте последовательность чисел. 
+#32. Задайте последовательность чисел. 
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
 import random
@@ -15,3 +15,24 @@ list_of_unique_numbers = [] # пустой список, для множеств
 for number in unique_numbers: # переводим множество в список 
     list_of_unique_numbers.append(number)
 print(list_of_unique_numbers)
+
+
+"""
+вариант 2
+subs = [1, 3, 5, 4, 9, 14, 5, 1]
+alone = []
+for i in range (0, len(subs)):
+duplicate = 0
+for j in range(0, len(subs)):
+if i != j:
+if subs[i] == subs[j]:
+duplicate = 1
+if duplicate == 0:
+alone.append(subs[i])
+print(alone)
+"""
+
+# вариант3 
+# numbers=list(map(int,input('Введите последовательность чисел через пробел: ').split()))
+
+# print("Cписок неповторяющихся элементов исходной последовательности: ", list(set(numbers)))
