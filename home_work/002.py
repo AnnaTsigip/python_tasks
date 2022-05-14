2a08515866178938ceef14b4e819997f7cbfdfda
Anna <atsigip@gmail.com>
2022-05-14 14:24:42 +0300

    list of prime factors of N

--- home_work/002.py
+++ home_work/002.py
@@ -14,4 +14,22 @@ def Factor(N):
         multipliers.append(N)
     return multipliers
 
-print(Factor(180))
+print(Factor(58984))
+
+
+"""
+N = int(input("Введите натуральное число N:\n"))
+List = []
+for i in range(2, N):
+while N % i == 0:
+List.append(i)
+N = N/i
+if N == 1:
+break
+
+if len(List) == 0:
+print("Число является простым")
+else:
+print("Cписок множителей числа N:")
+print(List)
+"""
