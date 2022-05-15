ae057347f8016f13c73729e77a8fa804d10c2bd5
Anna <atsigip@gmail.com>
2022-05-15 17:08:35 +0300

    game number 002 - 2 player, 002_2 - player/bot, 002_3 - player/smart bot

--- home_work_2/002_3.py
+++ home_work_2/002_3.py
@@ -5,7 +5,8 @@
 
 # b) Подумайте, как наделить бота "интеллектом"
 
-
+"""
+если бот ходит вторым, как сделать, чтоб не брал 0 конфет
 candies = int(input('Сколько конфет: '))
 take = int(input("Максимальное количество конфет можно взять: "))
 
@@ -31,3 +32,32 @@ if count % 2 != 0:
     print(f'Победил игрок player')
 else:
     print(f'Победил игрок bot')
+
+"""
+
+candies = int(input('Сколько конфет: '))
+take = int(input("Максимальное количество конфет можно взять: "))
+
+# сколько нужно взять конфет для победы: 
+def take_to_have_for_first(candies, take):
+     take_candies = candies % (take + 1)
+     return take_candies
+
+count = 0
+
+while candies > take:
+    count += 1
+    bot = take_to_have_for_first(candies, take)
+    print(f'bot взял {bot} конфет')
+    candies = candies - int(bot)
+    player = int(input("Ход первого игрока. Возьмите конфеты: "))
+    candies = candies - player
+    print(f'осталось конфет: {candies}')
+    #print(take_to_have_for_first(candies, take))
+
+if candies < take:
+    print('Игра окончена')
+if count % 2 != 0:
+    print(f'Победил игрок bot')
+else:
+    print(f'Победил игрок player')
