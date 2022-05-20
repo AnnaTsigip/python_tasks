a6146fbb372efb2a2b7b565fbd957ed382f6363a
Anna <atsigip@gmail.com>
2022-05-20 21:35:05 +0300

    набор произведений чисел от 1 до N

--- home_work_3/004.py
+++ home_work_3/004.py
@@ -1,15 +1,34 @@
  # Написать программу получающую набор произведений чисел от 1 до N.
 # Пример: пусть N = 4, тогда [ 1, 2, 6, 24 ]
-import os
+
+# list = []
+# num=1
+# product =1
+# N = int(input('N = '))
+# for i in range(1, N+1):
+#     num = num *i
+#     product = num
+#     list.append(product)
+# print(list)
+
+'''
+
+#Произведение:
+# from math import prod
+
+# n=int(input('Введите последнее число=')) 
+# print(prod([i for i in range(1,n+1)]))
+# #'Сумма чисел от 1 до, n, =, 
+'''
+
+
 from itertools import accumulate
 import operator
-import random
-os.system("cls")
 
-n = random.randint(8, 16)
 
-print(' Задать последовательность из', n, 'элементов\n',
-      'Последовательность:', *list(accumulate([x for x in range(1, n + 1)], operator.mul)), '\n')
+n = int(input('n = '))
+
+print(f'Набор произведений чисел от 1 до {n}: {list(accumulate([x for x in range(1, n + 1)], operator.mul))}')
 
 
 
