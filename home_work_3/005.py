495c45353cf7d709df267de49301e70c63128d9e
Anna <atsigip@gmail.com>
2022-05-20 22:03:37 +0300

    сумма чисел последовательности 1+(1/n)^n

--- home_work_3/005.py
+++ home_work_3/005.py
@@ -1,34 +1,16 @@
 # задать список из n чисел последовательности (1+〖1/n)〗^n и вывести на экран их сумму
 
-import os
-import random
-os.system("cls")
 
-n = random.randint(6, 16)
-
-num = [1+(1/i)**i for i in range(1, n+1)]
-print('Задана последовательность из ', n,
-      'чисел:\n', *num, '\nСумма чисел = ', round(sum(num), 2), '\n')
-
+# list = []
+# N = int(input('N = '))
+# for i in range(1, N+1):
+#     num = (1+(1/i))**i
+#     num_list = num
+#     list.append(num_list)
+#print(list)
 
 
-# 17. Задать список из N элементов, заполненных числами из [-N, N]. 
-# Найти произведение элементов на указанных позициях. 
-# Позиции хранятся в файле file.txt в одной строке одно число.
-
-N = int(input('Введите число' + '\n'))
-list = [i for i in range(-N,N+1)]
-path = '17.txt'
-data = open(path,'r')
-count = 1
-for line in data:
-    if int(line) > len(list)-1:
-        count*=1
-    else:    
-        count *= list[int(line)]
-print(count)
-data.close()
-
-
-
+n = int(input('Сколько элементов в последоватльности? \n'))
+number = [1+(1/i)** i for i in range(1, n+1)]
+print(f'Сумма {n} элементов последовательности = {round(sum(number),3)}')
 
